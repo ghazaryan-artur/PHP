3af2ca90c4986e6ca2c492a74ba85a0e63e10dde
name <ghazaryan.artur1@gmail.com>
2020-05-25 16:20:14 +0400

    add sql

--- registration/registration.sql
+++ registration/registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 25, 2020 at 10:45 AM
+-- Generation Time: May 25, 2020 at 12:19 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
@@ -45,7 +45,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `image`, `password`, `date`) VALUES
 (27, 'Artak', 'artak.amirkhanyan@coderiders.am', '0.54720700 1590403480.jpg', '698d51a19d8a121ce581499d7b701668', '2020-05-25 10:44:40'),
-(28, 'Artur', 'ghazaryan.artur1@gmail.com', '0.42687300 1590403503.jpg', 'bcbe3365e6ac95ea2c0343a2395834dd', '2020-05-25 10:45:03');
+(28, 'Artur', 'ghazaryan.artur1@gmail.com', '0.42687300 1590403503.jpg', 'bcbe3365e6ac95ea2c0343a2395834dd', '2020-05-25 10:45:03'),
+(29, 'Vlo', 'cerber1993@rambler.ru', '0.08951200 1590406088.jpg', '698d51a19d8a121ce581499d7b701668', '2020-05-25 11:28:08');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
